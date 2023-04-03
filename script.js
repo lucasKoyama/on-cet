c22aec13b0981bb467fbc5850e4bdf34124ae431
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-04-03 13:47:00 -0300

    parceiro incluido, descrições detalhadas do cesar e parceiros incluido

--- script.js
+++ script.js
@@ -43,21 +43,88 @@ main.addEventListener('scroll', () => {
 });
 
 // BIM Info
-
 const bim = document.querySelector('.bim');
 const bimInfo = document.querySelector('.bim-info');
 const aboutCetSec = document.querySelector('.about-on-cet');
 const partnersSec = document.querySelector('.partners');
 const returnBtn = document.querySelector('.bim-info button');
-const bimInfoState = (winOpacity, blur) => {
-  bimInfo.style.zIndex = winOpacity === '1' ? '1' : '-1';
-  bimInfo.style.opacity = winOpacity;
-  aboutCetSec.style.filter = blur;
-  partnersSec.style.filter = blur;
+const focusInfo = (blurSec, focusInfo, winOpacity, blur) => {
+  focusInfo.style.zIndex = winOpacity === '2' ? '2' : '-1';
+  focusInfo.style.opacity = winOpacity;
+  blurSec.style.filter = blur;
 };
-bim.addEventListener('click', () => bimInfoState('1', 'blur(4px)'));
-returnBtn.addEventListener('click', () => bimInfoState('0', ''));
+bim.addEventListener('click', () => focusInfo(aboutCetSec, bimInfo, '2', 'blur(4px)'));
+bim.addEventListener('click', () => focusInfo(partnersSec, bimInfo, '2', 'blur(4px)'));
+returnBtn.addEventListener('click', () => focusInfo(aboutCetSec, bimInfo, '0', ''));
+returnBtn.addEventListener('click', () => focusInfo(partnersSec, bimInfo, '0', ''));
 
+// Partner Info
+const partners = document.querySelectorAll('.partner');
+partners.forEach((partner) => partner.addEventListener('click', (event) => {
+  const info = partner.querySelector('.partner-info');
+  if (!event.target.classList.contains('return-btn') && info.style.display !== 'block') {
+    partners.forEach((person) => person.style.opacity = '0');
+    setTimeout(() => {
+      partner.childNodes[1].childNodes[3].classList.add('more-info-img');
+      partner.querySelectorAll('h4').forEach((h4) => h4.classList.add('more-info-h4'));
+      partner.childNodes[1].childNodes[5].classList.add('more-info-p');
+      info.style.display = 'block'
+      partners.forEach((person) => person.style.display = 'none');
+      partner.style.display = 'flex';
+      partner.style.opacity = '1';
+    }, 1400);
+    setTimeout(() => {
+      info.style.opacity = '1';
+      info.classList.add('partner-info-show');
+    }, 1600);
+    const btn = partner.querySelector('.return-btn');
+    btn.addEventListener('click', () => {
+      partners.forEach((person) => {
+        partner.style.opacity = '0';
+        setTimeout(() => {
+          partner.childNodes[1].childNodes[3].classList.remove('more-info-img');
+          partner.querySelectorAll('h4').forEach((h4) => h4.classList.remove('more-info-h4'));
+          partner.childNodes[1].childNodes[5].classList.remove('more-info-p');
+          info.style.display = 'none';
+          info.style.opacity = '0';
+          info.classList.remove('partner-info-show');
+          person.style.display = 'flex';
+        }, 1500)
+        setTimeout(() => person.style.opacity = '1', 1600);
+      });
+    });
+  }
+  }
+));
+
+// Vitae do Cesar
+const aboutMeBtn = document.querySelector('.about-me-more button');
+const generalInfo = document.querySelector('.info');
+const previousInfo = generalInfo.cloneNode(true);
+let notClicked = true;
+aboutMeBtn.addEventListener('click', () => {
+  if (notClicked) {
+    generalInfo.childNodes.forEach((ul, index) => {
+      if (index % 2 !== 0) ul.style.filter = 'blur(500px)';
+    });
+    generalInfo.style.background = 'rgb(28, 37, 64)';
+    setTimeout(() => {
+      generalInfo.innerHTML = '<p>César Tavares fundador da “Cet Engenharia”, criou um escritório de projetos estrutrais, sempre com o objetivo de criar a boa engenharia na construções civil. Buscando parcerias solidas e comprometidas. Atendendo São João da Boa Vista e Região. Hoje a Cet Engenharia evoluiu para “on cet”, com o objetivo de estender as fronteiras que o atendimento presencial limitava, para o atendimento on-line, rompendo as essas barreiras físicas e atender para todo território nacional, estamos 100 % com atendimento ON LINE.</p>';
+      generalInfo.classList.add('vitae');
+    }, 1500);
+    notClicked = false;
+  } else {
+    generalInfo.classList.remove('vitae');
+    generalInfo.style.background = 'rgb(243,154,52)';
+    generalInfo.innerHTML = previousInfo.innerHTML;
+    setTimeout(() => {
+      generalInfo.childNodes.forEach((ul, index) => {
+        if (index % 2 !== 0) ul.classList.add('show');
+      });
+    }, 200)
+    notClicked = true;
+  }
+});
 
 // Carousel - https://www.youtube.com/watch?v=QAD0K112tlQ&t=60s - BUG no último item a tela se desloca, provavelmente por conta do main ser o scroll principal
 const controls = document.querySelectorAll(".control");
@@ -99,20 +166,6 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
-
-// Partner
-const partners = document.querySelectorAll('.partner');
-const partnerInfo = () => {
-  partners.forEach((partner) => partner.style.opacity = '0');
-  partners[2].style.opacity = '1';
-  partners[2].style.transform = 'translate(-230px, -10px)';
-  setTimeout(() => {
-    partners[2].childNodes[1].childNodes[1].style.transform = 'translate(200px, 55px)';
-    partners[2].childNodes[1].childNodes[5].style.transform = 'translate(200px, -235px)';
-  }, 2500);
-};
-partners[2].addEventListener('click', partnerInfo);
-console.log(partners[2].childNodes);
 
 // Navbar no celular
 class MobileNavbar {
